7a0f8ba7daf0901847d817fede3e6bd1de8186b3
AndreLeme <62617416+AndreLeme@users.noreply.github.com>
2020-07-19 18:35:06 -0300

    revisao e style

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,6 +1,6 @@
-var pais = 'Brasil'
+var pais = 'eua'
 console.log(`Vivendo em ${pais}`)
-if (pais = 'Brasil') {
+if (pais == 'Brasil') {
     console.log('vc é brasileiro')
 } else {
     console.log('vc é estrangeiro')
